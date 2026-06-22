d8ce7af7e031fb5f1883e7cb001508baa34a43b1
Fabrice Daugan <fabrice.daugan@gmail.com>
2026-06-22 22:20:42 +0200

    chore: handle heterogeneous signed plugin vendors

--- src/test/java/org/ligoj/app/plugin/jenkins/JenkinsPluginResourceTest.java
+++ src/test/java/org/ligoj/app/plugin/jenkins/JenkinsPluginResourceTest.java
@@ -1,7 +1,7 @@
 /*
  * Licensed under MIT (https://github.com/ligoj/ligoj/blob/master/LICENSE)
  */
-package org.ligoj.app.plugin.build.jenkins;
+package org.ligoj.app.plugin.jenkins;
 
 import com.github.tomakehurst.wiremock.client.WireMock;
 import jakarta.transaction.Transactional;
